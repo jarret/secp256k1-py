4f976d5aa2e2ecd6a9ad8e3c2855401d1b81e7bb
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2021-09-08 15:08:53 -0600

    move coincurve keys to separate file

--- tests/test_bolt8.py
+++ tests/test_bolt8.py
@@ -5,77 +5,14 @@ from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from hashlib import sha256
-import os
-import coincurve
+
+from coincurve_keys import PrivateKey
+from coincurve_keys import PublicKey
 
 #############################################################################
 # cosmetically different, but nitty gritty borrowed from:
 #   https://github.com/ElementsProject/lightning/blob/master/contrib/pyln-proto/pyln/proto/wire.py
 #############################################################################
-
-#############################################################################
-# coincurve-depends stuff
-#############################################################################
-
-class PrivateKey(object):
-    def __init__(self, rawkey) -> None:
-        if not isinstance(rawkey, bytes):
-            raise TypeError(f"rawkey must be bytes, {type(rawkey)} received")
-        elif len(rawkey) != 32:
-            raise ValueError(
-                f"rawkey must be 32-byte long. {len(rawkey)} received")
-
-        self.rawkey = rawkey
-        self.key = coincurve.PrivateKey(rawkey)
-
-    def to_bytes(self):
-        return self.rawkey
-
-    def to_hex(self):
-        return self.to_bytes().hex()
-
-    def public_key(self):
-        return PublicKey(self.key.public_key)
-
-    def ecdh(self, pubkey):
-        k = coincurve.PrivateKey(secret=self.to_bytes())
-        rk = coincurve.PublicKey(data=pubkey.to_bytes())
-        return k.ecdh(rk.public_key)
-
-    @staticmethod
-    def new_ephemeral():
-        return PrivateKey(os.urandom(32))
-
-    @staticmethod
-    def from_hex(hex_str):
-        assert len(hex_str) == 64
-        return PrivateKey(bytes.fromhex(hex_str))
-
-
-class PublicKey(object):
-    def __init__(self, innerkey):
-        # We accept either 33-bytes raw keys, or an EC PublicKey as returned
-        # by coincurve
-        if isinstance(innerkey, bytes):
-            if innerkey[0] in [2, 3] and len(innerkey) == 33:
-                innerkey = coincurve.PublicKey(innerkey)
-            else:
-                raise ValueError("Byte keys must be 33-byte long starting "
-                                 "from either 02 or 03")
-        elif not isinstance(innerkey, coincurve.keys.PublicKey):
-            raise ValueError("Key must either be bytes or "
-                             "coincurve.keys.PublicKey")
-        self.key = innerkey
-
-    def to_bytes(self) -> bytes:
-        return self.key.format(compressed=True)
-
-    def to_hex(self):
-        return self.to_bytes().hex()
-
-    def __str__(self):
-        return "PublicKey[0x{}]".format(self.to_bytes().hex())
-
 
 #############################################################################
 # Bolt8 implementation
